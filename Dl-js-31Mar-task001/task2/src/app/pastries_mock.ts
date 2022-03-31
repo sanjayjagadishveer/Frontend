56203238acd54414c83ab4b5f617bb6f8e4c9e21
sanjayjagadishveer <sanjayjagadish8@gmail.com>
2022-03-31 21:14:48 +0530

    add my files

---
+++ Dl-js-31Mar-task001/task2/src/app/pastries_mock.ts
@@ -0,0 +1,8 @@
+
+export const pastries=[
+    {name:'pie',path:'../../assets/pexels-photo-808941.jpeg'},
+    {name:'Croissants',path:'../../assets/img2.jpg'},
+    {name:'Danishes',path:'../../assets/img3.jpg'},
+    {name:'Macarons',path:'../../assets/img4.jpg'},
+]
+   
